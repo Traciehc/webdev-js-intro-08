d3824f75c77b159d8802b25b89a11f648f0a1680
Tracie Childers <traciechilders01@gmail.com>
2024-12-29 21:48:26 -0500

    Updated README.md and created function findSmallestNumber in assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -5,4 +5,24 @@ const smallestNumberElement = document.getElementById("smallest-number");
 const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
+
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+function findSmallestNumber(numbers) {
+  let smallest = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < smallest) {
+      smallest = numbers[i];
+    }
+  }
+  return smallest;
+}
+
+console.log(findSmallestNumber(myNumbers)); // This will output: 25
+
+
+
+
+
+
+
